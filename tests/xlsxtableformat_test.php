16873ed119c796559ed6339f6b6303462ed6d01d
Maks <mark.merlin2003@gmail.com>
2023-12-30 10:33:06 +0300

    Update line in tests/xlsxtableformat_test.php: // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.

--- tests/xlsxtableformat_test.php
+++ tests/xlsxtableformat_test.php
@@ -61,3 +61,4 @@ class qformat_wordtable_export_test extends question_testcase {
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
