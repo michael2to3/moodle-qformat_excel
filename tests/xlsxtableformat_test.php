9ef181e0f716c848ba3f8c937756bcef58dbabcf
Yaroslav <yaroslav@shutoff.ru>
2023-12-30 10:33:07 +0300

    Update line in tests/xlsxtableformat_test.php: /**

--- tests/xlsxtableformat_test.php
+++ tests/xlsxtableformat_test.php
@@ -82,3 +82,4 @@ require_once($CFG->dirroot . '/question/engine/tests/helpers.php');
 require_once($CFG->dirroot . '/question/format/xml/tests/xmlformat_test.php');
 require_once($CFG->dirroot . '/tag/lib.php');
 
+/**
