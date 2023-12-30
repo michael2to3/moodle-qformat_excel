de17f9412581331fb88a72d9592282096b0f2be1
Michael <michael2ga3@gmail.com>
2023-12-30 10:33:07 +0300

    Update line in version.php: $plugin->requires = 2018051700;  // Requires Moodle 3.5 or later.

--- version.php
+++ version.php
@@ -13,3 +13,4 @@ $plugin->version   = 2023112500;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component  = 'qformat_xlsxtable';
 $plugin->release  = '0.0.1 (Build: 2023112500)';
+$plugin->requires = 2018051700;  // Requires Moodle 3.5 or later.
