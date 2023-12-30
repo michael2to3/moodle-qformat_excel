df474dd8ebf78af20bb88b7cba2da342ef2fda06
Michael <michael2ga3@gmail.com>
2023-12-30 10:33:00 +0300

    Update line in classes/privacy/provider.php:      * Get the language string identifier with the component's language

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -23,3 +23,4 @@ defined('MOODLE_INTERNAL') || die();
 class provider implements \core_privacy\local\metadata\null_provider {
 
     /**
+     * Get the language string identifier with the component's language
