b935160a1803719097c22170f58f2fdbb741593c
Alesha <aleshadmi@mail.ru>
2023-12-30 10:33:02 +0300

    Update line in format.php:                 $q->tolerance = [$fine];

--- format.php
+++ format.php
@@ -396,3 +396,4 @@ class qformat_xlsxtable extends qformat_default
 
                 $q->fraction  = [$fraction];
                 $q->answer    = [$keys['_answer'][$i]];
+                $q->tolerance = [$fine];
