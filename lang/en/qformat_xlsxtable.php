1fa4cd3715cacabd69ae0ff4b1d8c1af909530e7
Yaroslav <yaroslav@shutoff.ru>
2023-12-30 10:33:05 +0300

    Update line in lang/en/qformat_xlsxtable.php: $string['htmlnotsupported'] = 'Files in HTML format not supported: <b>{$a}</b>';

--- lang/en/qformat_xlsxtable.php
+++ lang/en/qformat_xlsxtable.php
@@ -57,3 +57,4 @@ $string['cannotreadzippedfile'] = 'Cannot read Zipped file <b>{$a}</b>';
 $string['cannotwritetotempfile'] = 'Cannot write to temporary file <b>{$a}</b>';
 $string['docnotsupported'] = 'Files in XLSX 2003 format not supported: <b>{$a}</b>, use Moodle2XLSX 3.x instead';
 $string['htmldocnotsupported'] = 'Incorrect XLSX format: please use <i>File>Save As...</i> to save <b>{$a}</b> in native XLSX 2010 (.xlsx) format and import again';
+$string['htmlnotsupported'] = 'Files in HTML format not supported: <b>{$a}</b>';
