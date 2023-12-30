9790670a4f1393e00ebd96690c2bbac77ee9776b
Michael <michael2ga3@gmail.com>
2023-12-30 10:33:06 +0300

    Update line in lang/ru/qformat_xlsxtable.php: $string['preview_question_not_found'] = 'Предварительный просмотр вопроса не найден, имя / ID курса: {$a}';

--- lang/ru/qformat_xlsxtable.php
+++ lang/ru/qformat_xlsxtable.php
@@ -57,3 +57,4 @@ $string['noquestions'] = 'Нет вопросов для экспорта';
 $string['pluginname'] = 'Формат таблицы Microsoft XLSX 2008';
 $string['pluginname_help'] = 'Это интерфейс для конвертации файлов Microsoft XLSX в формат XML вопросов Moodle для импорта, а также для конвертации формата XML вопросов Moodle в формат, подходящий для редактирования в Microsoft XLSX.';
 $string['pluginname_link'] = 'qformat/xlsxtable';
+$string['preview_question_not_found'] = 'Предварительный просмотр вопроса не найден, имя / ID курса: {$a}';
