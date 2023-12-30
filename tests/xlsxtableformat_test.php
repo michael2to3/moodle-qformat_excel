53f9815e3562df2160a79f70cc9eee183c6ff932
Yunus <yunus.asadulaev@mail.ru>
2023-12-30 10:33:07 +0300

    Update line in tests/xlsxtableformat_test.php:  * @group qformat_wordtable

--- tests/xlsxtableformat_test.php
+++ tests/xlsxtableformat_test.php
@@ -89,3 +89,4 @@ require_once($CFG->dirroot . '/tag/lib.php');
  * the qformat_wordtable::presave_process method, and then compared to the expected output.
  * @copyright  2016 Eoin Campbell
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @group qformat_wordtable
