15949a2ac5f11e0075ed717475c51d47d03066c1
Michael <michael2ga3@gmail.com>
2023-12-30 10:33:00 +0300

    Update line in classes/privacy/provider.php:      *

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -25,3 +25,4 @@ class provider implements \core_privacy\local\metadata\null_provider {
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
+     *
