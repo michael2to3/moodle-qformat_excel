4a9e3af75834d7227386f35481a7d525d6821f20
Michael <michael2ga3@gmail.com>
2023-12-30 10:33:06 +0300

    Update line in tests/xlsxtableformat_test.php: // You should have received a copy of the GNU General Public License

--- tests/xlsxtableformat_test.php
+++ tests/xlsxtableformat_test.php
@@ -60,3 +60,4 @@ class qformat_wordtable_export_test extends question_testcase {
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
+// You should have received a copy of the GNU General Public License
