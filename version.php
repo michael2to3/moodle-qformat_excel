291963bc201c64c9cda2ddde3710a6ca57a225cc
Michael <michael2ga3@gmail.com>
2023-12-30 10:33:07 +0300

    Update line in version.php: defined('MOODLE_INTERNAL') || die();

--- version.php
+++ version.php
@@ -7,3 +7,4 @@ $plugin->component  = 'qformat_xlsxtable';
 $plugin->release  = '0.0.1 (Build: 2023112500)';
 $plugin->requires = 2018051700;  // Requires Moodle 3.5 or later.
 <?php
+defined('MOODLE_INTERNAL') || die();
