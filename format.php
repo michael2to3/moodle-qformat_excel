10213ae0f50402f82ae8e3b0300f28f27d47d89d
Michael <michael2ga3@gmail.com>
2023-12-30 10:33:02 +0300

    Update line in format.php:                 $q->name         = $name;

--- format.php
+++ format.php
@@ -380,3 +380,4 @@ class qformat_xlsxtable extends qformat_default
 
                 $q               = $this->defaultquestion();
                 $q->id           = $i;
+                $q->name         = $name;
