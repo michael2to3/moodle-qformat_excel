36fc74129c696d9bb9d20da08506a9b1daeb25e9
Yaroslav <yaroslav@shutoff.ru>
2023-12-30 10:33:07 +0300

    Update line in version.php: <?php

--- version.php
+++ version.php
@@ -6,3 +6,4 @@ $plugin->maturity  = MATURITY_STABLE;
 $plugin->component  = 'qformat_xlsxtable';
 $plugin->release  = '0.0.1 (Build: 2023112500)';
 $plugin->requires = 2018051700;  // Requires Moodle 3.5 or later.
+<?php
