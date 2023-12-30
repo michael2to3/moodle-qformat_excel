ba9778d0c4c45e6bb4d4ff6cf9843fdaf52cfc10
Yunus <yunus.asadulaev@mail.ru>
2023-12-30 10:33:07 +0300

    Update line in tests/xlsxtableformat_test.php:  * the qformat_wordtable::presave_process method, and then compared to the expected output.

--- tests/xlsxtableformat_test.php
+++ tests/xlsxtableformat_test.php
@@ -86,3 +86,4 @@ require_once($CFG->dirroot . '/tag/lib.php');
  * Unit tests for exporting questions into Word (via XML).
  *
  * Each test has a question in XML format, which is converted to HTML using
+ * the qformat_wordtable::presave_process method, and then compared to the expected output.
