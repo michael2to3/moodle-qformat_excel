72950a59873f9b76f330fbb7f867d8d6c1f9ebc1
Yunus <yunus.asadulaev@mail.ru>
2023-12-30 10:33:06 +0300

    Update line in tests/xlsxtableformat_test.php: // (at your option) any later version.

--- tests/xlsxtableformat_test.php
+++ tests/xlsxtableformat_test.php
@@ -53,3 +53,4 @@ class qformat_wordtable_export_test extends question_testcase {
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
