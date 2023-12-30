b52140b2e791284354c79f6ca11d95efd2e4fc02
Alesha <aleshadmi@mail.ru>
2023-12-30 10:33:06 +0300

    Update line in lang/ru/qformat_xlsxtable.php: $string['xlsx_new_question_file'] = 'Новый файл вопроса';

--- lang/ru/qformat_xlsxtable.php
+++ lang/ru/qformat_xlsxtable.php
@@ -84,3 +84,4 @@ $string['xlsx_currentquestion'] = ' (Текущий вопрос)';
 $string['xlsx_gapselect_screentip'] = 'Предупреждение: для этого типа вопроса требуется настроенный плагин Moodle для выбора пропущенных слов.';
 $string['xlsx_import'] = 'Импорт';
 $string['xlsx_multiple_answer'] = 'Несколько ответов';
+$string['xlsx_new_question_file'] = 'Новый файл вопроса';
