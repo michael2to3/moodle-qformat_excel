e013b04c4115513ea44c73330a6f84040263efca
Yunus <yunus.asadulaev@mail.ru>
2023-12-30 10:33:06 +0300

    Update line in lang/en/qformat_xlsxtable.php: $string['xlsx_shuffle_supertip'] = 'A few shuffles is better than 1';

--- lang/en/qformat_xlsxtable.php
+++ lang/en/qformat_xlsxtable.php
@@ -100,3 +100,4 @@ $string['xlsx_showhide_assessment_screentip'] = 'Show question metadata to edit,
 $string['xlsx_showhide_assessment_supertip'] = 'Shows or hides the hidden text';
 $string['xlsx_showhide_assessment_view'] = 'Show/Hide Assessment View';
 $string['xlsx_shuffle_screentip'] = 'Shuffle the answers to MCQ/TF/MA questions';
+$string['xlsx_shuffle_supertip'] = 'A few shuffles is better than 1';
