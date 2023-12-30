c7196b4d9f6f48e924ec61cb705209c8140f2531
Michael <michael2ga3@gmail.com>
2023-12-30 10:33:05 +0300

    Update line in lang/en/qformat_xlsxtable.php: $string['xlsx_gapselect_screentip'] = 'Warning: customised Select missing xlsxs Moodle plugin required for this question type.';

--- lang/en/qformat_xlsxtable.php
+++ lang/en/qformat_xlsxtable.php
@@ -89,3 +89,4 @@ $string['xlsx_about_moodle2xlsx'] = 'About Moodle2XLSX';
 $string['xlsx_about_moodle2xlsx_screentip'] = 'About the Moodle2XLSX XLSX templates and Moodle plug-in';
 $string['xlsx_addcategory_supertip'] = 'Category names use the Heading 1 style';
 $string['xlsx_currentquestion'] = ' (Current Question)';
+$string['xlsx_gapselect_screentip'] = 'Warning: customised Select missing xlsxs Moodle plugin required for this question type.';
